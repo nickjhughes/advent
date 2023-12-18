f41c5f1f830273e50efd4b1d47bc872dc6458f3a
Nick Hughes <689543+nickjhughes@users.noreply.github.com>
2023-12-18 13:33:35 +1100

    2023 day 17 part 2

--- 2023/src/days/day_17.rs
+++ 2023/src/days/day_17.rs
@@ -1,5 +1,6 @@
-use hashbrown::{HashMap, HashSet};
-use std::fs;
+use hashbrown::HashMap;
+use priority_queue::PriorityQueue;
+use std::{cmp::Reverse, fs};
 
 pub fn part1() -> String {
     let input = get_input_file_contents();
@@ -65,63 +66,19 @@ impl Map {
     }
 
     fn minimal_heat_loss(&self, min_steps: u8, max_steps: u8) -> u32 {
-        let mut open_set: HashSet<SearchNode> = HashSet::new();
-        open_set.insert(SearchNode::new(0, Dir::Right, 0));
+        let mut open_set = PriorityQueue::new();
+        open_set.push(SearchNode::new(0, Dir::Right, 0), Reverse(0));
 
         let mut heat_loss: HashMap<SearchNode, u32> = HashMap::new();
         heat_loss.insert(SearchNode::new(0, Dir::Right, 0), 0);
 
-        let mut prev: HashMap<SearchNode, SearchNode> = HashMap::new();
-
         while !open_set.is_empty() {
-            let mut min_node = None;
-            let mut min_heat_loss = u32::MAX;
-            for node in open_set.iter() {
-                if let Some(hl) = heat_loss.get(node) {
-                    if *hl < min_heat_loss {
-                        min_node = Some(*node);
-                        min_heat_loss = *hl;
-                    }
-                }
-            }
-            let current = open_set.take(&min_node.unwrap()).unwrap();
+            let (current, _) = open_set.pop().unwrap();
             let current_row = current.index / self.width;
             let current_col = current.index % self.width;
 
             if current_col == self.width - 1 && current_row == self.height() - 1 {
                 // Reached goal
-
-                // Goal reached, reconstruct path
-                let mut path = Vec::new();
-                path.push(current);
-                let mut node = current;
-                while let Some(prev) = prev.get(&node) {
-                    path.push(*prev);
-                    node = *prev;
-                }
-
-                // Print grid + path
-                // for row in 0..self.height() {
-                //     for col in 0..self.width {
-                //         if let Some(node) = path.iter().find(|n| n.index == row * &self.width + col)
-                //         {
-                //             print!(
-                //                 "{}",
-                //                 match node.dir {
-                //                     Dir::Up => "^",
-                //                     Dir::Down => "v",
-                //                     Dir::Left => "<",
-                //                     Dir::Right => ">",
-                //                 }
-                //             );
-                //         } else {
-                //             print!("{}", self.blocks[row * self.width + col]);
-                //         }
-                //     }
-                //     println!();
-                // }
-                // println!();
-
                 return *heat_loss.get(&current).unwrap();
             }
 
@@ -148,7 +105,7 @@ impl Map {
             let down = if current_row < self.height() - 1
                 && current.dir != Dir::Up
                 && !(current.dir == Dir::Down && current.steps == max_steps)
-                && !(current.dir != Dir::Down && current.steps < min_steps)
+                && (!(current.dir != Dir::Down && current.steps < min_steps) || current.index == 0)
                 && !(min_steps > 1
                     && current_row == self.height() - 2
                     && current_col == self.width - 1
@@ -207,14 +164,11 @@ impl Map {
                 None
             };
             for neighbor in [up, down, left, right].into_iter().flatten() {
-                let tentative_dist =
+                let tentative_heat_loss =
                     heat_loss.get(&current).unwrap() + self.blocks[neighbor.index] as u32;
-                if tentative_dist < *heat_loss.get(&neighbor).unwrap_or(&u32::MAX) {
-                    prev.insert(neighbor, current);
-                    heat_loss.insert(neighbor, tentative_dist);
-                    if !open_set.contains(&neighbor) {
-                        open_set.insert(neighbor);
-                    }
+                if tentative_heat_loss < *heat_loss.get(&neighbor).unwrap_or(&u32::MAX) {
+                    heat_loss.insert(neighbor, tentative_heat_loss);
+                    open_set.push(neighbor, Reverse(tentative_heat_loss));
                 }
             }
         }
@@ -247,6 +201,19 @@ fn test_minimal_heat_loss_ultra() {
 
     {
         let input = "111111111111\n999999999991\n999999999991\n999999999991\n999999999991\n";
+        let map = Map::parse(input);
+        assert_eq!(map.minimal_heat_loss(4, 10), 71);
+    }
+
+    {
+        let input = "111111111111\n999999999991\n999999999991\n999999999991\n999999999991\n";
+        let map = Map::parse(input);
+        assert_eq!(map.minimal_heat_loss(4, 10), 71);
+    }
+
+    {
+        let input =
+            "19999\n19999\n19999\n19999\n19999\n19999\n19999\n19999\n19999\n19999\n19999\n11111\n";
         let map = Map::parse(input);
         assert_eq!(map.minimal_heat_loss(4, 10), 71);
     }
