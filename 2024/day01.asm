79b76816a8dd6d096604e9211c1a7f1f825a7d9d
Nick Hughes <689543+nickjhughes@users.noreply.github.com>
2024-10-11 12:34:09 +1100

    Successfully counting the number of lines in the input in assembly

--- 2024/day01.asm
+++ 2024/day01.asm
@@ -1,20 +1,81 @@
 .code64
+
 .section .rodata
-msg: .ascii "Hello, World!\n"
-.set msglen, (. - msg)
+input: .ascii "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n"
+.set input_end, .
+.set input_len, (input_end - input)
+
+.section .data
+output: .ascii "        \n"
+.set output_len, (. - output)
 
 .section .text
+
 .global _start
 _start:
 
-# write(2)
-mov $1, %rax
-mov $1, %rdi
-lea msg, %rsi
-mov $msglen, %rdx
-syscall
+# Initialize sum to 0
+movq $0, %rax
 
-# exit(2)
-mov $60, %rax
-mov $0, %rdi 
-syscall
+# Start at beginning of input
+lea input, %r11
+
+# Loop over each character
+char_loop:
+  # Load char into register
+  movb (%r11), %cl
+  movb %cl, (output)
+  
+  # Check if char is a newline, and if so jump to end_of_line
+  cmp $10, %cl
+  je end_of_line
+  
+  # Otherwise, check if char is a digit
+  # TODO
+  
+next_char:
+  # Move to next char
+  inc %r11
+  # If we're at the end of the input, jump to end_of_input, otherwise continue
+  cmp $input_end, %r11
+  je end_of_input
+  jmp char_loop
+
+end_of_line:
+  # TODO: Construct number from the two digits and add to running sum
+  #       For now just count the number of lines
+  inc %rax
+  jmp next_char
+
+end_of_input:
+  # TODO: Print out sum as answer
+  #       For now just print out the number of lines, assuming it's less than 10
+  add $48, %al
+  movb %al, (output)
+  call print_output
+  jmp exit
+
+print_output:
+  push %rax
+  push %rdi
+  push %rsi
+  push %rdi
+  push %r11
+  push %rcx
+  mov $1, %rax
+  mov $1, %rdi
+  lea output, %rsi
+  mov $output_len, %rdx
+  syscall
+  pop %rcx
+  pop %r11
+  pop %rdi
+  pop %rsi
+  pop %rdi
+  pop %rax
+  ret
+
+exit:
+  mov $60, %rax
+  mov $0, %rdi 
+  syscall
--- 2024/run.ps1
+++ 2024/run.ps1
@@ -1,3 +1,3 @@
 param ([Parameter(Mandatory=$true)][string]$day)
 echo "Running day $day"
-docker build -t aoc-2024 --quiet --build-arg DAY="$day" . && docker run aoc-2024
+docker build -t aoc-2024 --build-arg DAY="$day" . && docker run -it aoc-2024
